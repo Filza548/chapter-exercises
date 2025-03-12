70097ff93f46d5dba3f4918ba16d7747fbf43b64
Filza548 <filzajameel458@gmail.com>
2025-03-13 00:36:23 +0500

    still working

---
+++ index.js
@@ -0,0 +1,101 @@
+// ..........................................CHAPTER NUMBER # 01 ANSWERS 
+
+// ................QUESTION NO # 1,2,3 ANSWER
+// alert("Hellow Everyone\n\nThank you for opening this website\nI hope apko meri yeah website achi lagi hogi")
+
+// .......................// QUESTION NO # 5 ANSWER
+// alert("hello... I can run js through my web browser's console")
+// console.log("hello... I can run js through my web browser's console");
+
+
+// .............................................CHAPTER NUMBER # 02 ANSWERS 
+
+// ...............................Question # 01 Answer 
+// let userName = ""
+
+// ...............................Question # 02 Answer 
+// let myName = "filza jameel"
+
+// ...............................Question # 03 (a) Answer
+// let message = "" 
+
+// ...............................Question # 03 (b) Answer
+// message = "Hello World "
+
+// ...............................Question # 03 (c) Answer
+// alert(message)
+
+// ...............................Question # 04 Answer
+// let name = "Noor"
+// alert ("yeah aik alert box hai ", name )
+// let age = 5
+// alert (age )
+// let rollNumber = 67899
+// alert ( rollNumber )
+ 
+// ...............................Question # 05 Answer
+// let pizzaVar = "pizza\npizz\npiz\npi\np"
+// alert(pizzaVar)
+
+// ...............................Question # 06 Answer
+// let email = "filzajameel458@gmail.com"
+// alert(email)
+
+// ...............................Question # 07 Answer
+// let book = "A smarter way to learn javascript"
+// alert("I am trying to learn from the Book" + " " + book )
+
+// ...............................Question # 08 Answer
+// document.write("<h1>" + "Yah! I can write HTML Content Through Javascript " + "</h1>")
+
+// ...............................Question # 09 Answer
+// let end = "-END-"
+// alert(end)
+
+
+// .............................................CHAPTER NUMBER # 03 ANSWERS 
+// ...............................Question # 01 Answer 
+// let age2 = "I am 22 years old "
+// alert(age2)
+
+// ...............................Question # 02 Answer
+// let visits = 0 
+
+// visits = visits + 14
+
+// alert ("You have visited this site " + visits + " times")
+
+// ...............................Question # 03 Answer
+// let birthYear = 2002
+// document.write("<h1>" + "My birth year " + birthYear + "</h1>")
+
+// ...............................Question # 04 Answer
+// let visitorName = "Ayan Adeel"
+// let productName = "T-shirts"
+// let productQuantity = 5
+
+// document.write("<h1>" + visitorName + " ne " + productQuantity + " "+ productName + " li hain Clothing Brands se  "
+//     + "</h1>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
